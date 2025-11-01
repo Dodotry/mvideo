1f549c7b063a34fbd4743c88a22e3e5725913f36
Dodotry <48266156+Dodotry@users.noreply.github.com>
2025-11-01 23:03:14 +0800

    Add script to update cn.conf with rule lists

---
+++ updatecnname.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+curl 'https://gh.xmly.dev/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt' > /opt/smartdns/cn.conf
+curl 'https://gh.xmly.dev/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt' >> /opt/smartdns/cn.conf
+curl 'https://gh.xmly.dev/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/google-cn.txt' >> /opt/smartdns/cn.conf
+curl 'https://gh.xmly.dev/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/china-list.txt' >> /opt/smartdns/cn.conf
+
+sed "s/^full://g;/^regexp:.*$/d;s/^/nameserver \//g;s/$/\/cn/g" -i /opt/smartdns/cn.conf
+
